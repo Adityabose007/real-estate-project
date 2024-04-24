c1c2fb7bc371eb633213c3d0edb78a07ac90e6a1
aditya bose <boseaditya680@gmail.com>
2024-04-25 01:56:44 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize:{
+        a:"0.75rem",
+        b:"0.875rem",
+        c:"1rem",
+        d:"1.25rem",
+        e:"1.5rem",
+        f:"1.75rem",
+        g:"2.25rem",
+      }
+    },
   },
   plugins: [],
 }
