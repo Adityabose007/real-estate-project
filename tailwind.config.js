be4c08c75c3ef2a34439c251db4f4fe254df7767
aditya bose <boseaditya680@gmail.com>
2024-04-27 22:22:27 +0530

    desktop 90% done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./*.{html,js}"],
@@ -11,7 +10,8 @@ export default {
         lg:"1.25rem",
         xl:"1.5rem",
         "2xl":"1.75rem",
-        "3xl":"2.25rem"
+        "3xl":"2.25rem",
+        "4xl":"3rem"
       },
 
       backgroundImage: {
